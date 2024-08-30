52076cb15c09502acf6334631feade8a679b6db7
Arthur Sonzogni <arthursonzogni@chromium.org>
2024-08-30 08:36:55 +0000

    Adapt to span.

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,72 @@
 #include <cstdlib>
 #include <iostream>
 #include "parser.hpp"
+#include <ranges>
+#include <algorithm>
+
+// C++20
+
+// For each pattern, we count the number of added/removed lines, in tests and
+// non tests.
+const auto patterns = std::to_array({
+    "#pragma allow_unsafe_buffers",
+    "UNSAFE_BUFFERS(",
+    "UNSAFE_TODO(",
+    "UNSAFE_BUFFERS_USAGE",
+  });
+
+struct Count {
+  int added = 0;
+  int removed = 0;
+  int added_test = 0;
+  int removed_test = 0;
+};
+
+bool is_empty(Count c) {
+  return c.added == 0 && c.removed == 0 && c.added_test == 0 && c.removed_test == 0;
+}
+
+std::array<Count, patterns.size()> count(Commit commit) {
+  std::array<Count, patterns.size()> result;
+
+  for (auto file : GetFiles(commit)) {
+    const bool is_test = file.right_file.find("test") != std::string::npos;
+    for (auto hunk : file.hunks) {
+      for (auto line : hunk.lines) {
+        for (size_t i = 0; i < patterns.size(); ++i) {
+          const auto& pattern = patterns[i];
+          if (line.content.find(pattern) == std::string::npos) {
+            continue;
+          }
+
+          switch (line.type) {
+            case Line::Keep:
+              break;
+            case Line::Add:
+              (is_test ? result[i].added_test : result[i].added)++;
+              break;
+            case Line::Delete:
+              (is_test ? result[i].removed_test : result[i].removed)++;
+              break;
+          }
+        }
+      }
+    }
+  }
+
+  // Normalize the count.
+  for (auto& c : result) {
+    int changed = std::min(c.added, c.removed);
+    c.added -= changed;
+    c.removed -= changed;
+
+    int changed_test = std::min(c.added_test, c.removed_test);
+    c.added_test -= changed_test;
+    c.removed_test -= changed_test;
+  }
+
+  return result;
+}
 
 int main(int argument_count, char** argument_values) {
   const std::string last_hash = (argument_count > 1) ? argument_values[1] : "";
@@ -14,53 +80,20 @@ int main(int argument_count, char** argument_values) {
   while (true) {
     ++i;
     auto commit = GetCommit(head);
-    int added = 0;
-    int removed = 0;
-    int added_test = 0;
-    int removed_test = 0;
-    for (auto file : GetFiles(commit)) {
-      const bool is_test = file.right_file.find("test") != std::string::npos;
-      for (auto hunk : file.hunks) {
-        for (auto line : hunk.lines) {
-          if (line.content.find("raw_") == std::string::npos) {
-            continue;
-          }
 
-          if (line.content.find("DanglingUntriaged") == std::string::npos &&
-              line.content.find("DisableDanglingPtrDetection") == std::string::npos) {
-            continue;
-          }
+    auto counts = count(commit);
 
-          switch (line.type) {
-            case Line::Keep:
-              break;
-            case Line::Add:
-              (is_test ? added_test : added)++;
-              break;
-            case Line::Delete:
-              (is_test ? removed_test : removed)++;
-              break;
-          }
-        }
-      }
-    }
-
-    int changed = std::min(added, removed);
-    added -= changed;
-    removed -= changed;
-
-    int changed_test = std::min(added_test, removed_test);
-    added_test -= changed_test;
-    removed_test -= changed_test;
-
-    if (added + removed + added_test + removed_test != 0) {
+    const bool all_counts_empty = std::ranges::all_of(counts, is_empty);
+    if (!all_counts_empty) {
       std::cout << commit.timestamp << "\t"         //
-                << commit.hash << "\t"       //
-                << added << "\t"             //
-                << removed << "\t"           //
-                << added_test << "\t"        //
-                << removed_test << "\t"      //
-                << commit.authors[0] << "\t" //
+                << commit.hash << "\t";       //
+      for (const Count& c : counts) {
+        std::cout << c.added << "\t"             //
+                  << c.removed << "\t"           //
+                  << c.added_test << "\t"        //
+                  << c.removed_test << "\t";
+      }
+      std::cout << commit.authors[0] << "\t" //
                 << commit.title << std::endl;
     }
 
